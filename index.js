2f134281d2cef399636ba0f16c677832d1c0219f
DescrySama <erwing2002@gmail.com>
2022-01-12 11:53:38 +0100

    nn

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ document.body.appendChild(section);
 
 for (let i = 1; i < 11; i++) {
     const div = document.createElement('div');
+    const img = document.createElement('img');
+    img.src = 'bonjour.jpg';
+    img.style.width = "10rem"
     div.className = 'card';
     div.setAttribute('id', 'lescards');
+    div.appendChild(img)
     section.appendChild(div);
   }
